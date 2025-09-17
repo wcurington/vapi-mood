b2c25c0d0f0f3cc67a95c8ec90c6b8dbcaa98ae5
William Curington <wcurington@gmail.com>
2025-09-16 19:34:55 -0500

    Remove node-fetch, use Node.js built-in fetch instead

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { google } = require("googleapis");
-const fetch = require("node-fetch");
 
 const app = express();
 app.use(bodyParser.json());
